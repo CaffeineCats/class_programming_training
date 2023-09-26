d77a01430e21ccb9c826eecd44f721018e9ca44b
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-09-25 21:14:07 -0300

    Prep 1 Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        // It seems like this is a comment.
+        // Variable names are Camel Case as a good practice I believe.
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Seems like a Console.WriteLine() will print an empty line, haven't found a way to \n;
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
