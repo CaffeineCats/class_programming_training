cf620d2a2192c7324ab21afe78190466885e472b
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-09-27 00:32:28 -0300

    Prep 3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int userGuess = 0;
+
+        do
+        {
+            Console.Write("What is your Guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower mah boy");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher mah boy");
+            }
+            else
+            {
+                Console.WriteLine("You got it right, U r a wizzard Harry, A WIZZARD.");
+            }
+        } while (userGuess != magicNumber);
     }
 }
