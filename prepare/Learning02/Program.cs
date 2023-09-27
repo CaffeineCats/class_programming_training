a7c373f5b771aa0f3c3927e04ce65b8c7321b748
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-09-27 16:38:17 -0300

    Completed Learning Activity 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume resume = new Resume();
+
+        resume._jobs.Add(new Job());
+        resume._jobs.Add(new Job());
+
+        resume._name = "Allison Rose";
+        resume._jobs[0]._company = "Microsoft";
+        resume._jobs[0]._jobTitle = "Software Engineer";
+        resume._jobs[0]._startYear = 2019;
+        resume._jobs[0]._endYear = 2022;
+
+        resume._jobs[1]._company = "Apple";
+        resume._jobs[1]._jobTitle = "Manager";
+        resume._jobs[1]._startYear = 2022;
+        resume._jobs[1]._endYear = 2023;
+
+        resume.DisplayResume();
     }
+
 }
