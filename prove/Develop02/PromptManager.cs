033aef0d63ed4177abf184f9bdaef8e650a8aa1d
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-10-06 13:14:02 -0300

    Prompt manager completed

--- prove/Develop02/PromptManager.cs
+++ prove/Develop02/PromptManager.cs
@@ -2,6 +2,10 @@ public class PromptManager
 {
     public List<string> _prompts;
 
+    //The purpose of the Prompt manager is to manage all the prompts that are feed to journal;
+    // That is why it has its own class, and is a member variable in Journal;
+    // If the user adds a prompt to the list or pops one (we would need a file for that), but the default ones are at the constructor;
+    // I want that list to be the same all across the program.
     public PromptManager()
     {
         _prompts = new List<string> 
@@ -23,4 +27,12 @@ public class PromptManager
 
         return _prompts[randomNumber];
     }
+    // Stretch challenge, sorry it's so simple.
+    public void AddUserPrompt()
+    {
+        Console.Write("Please write your prompt: ");
+        string newPrompt = Console.ReadLine();
+        _prompts.Add(newPrompt);
+    }
+    // We would need a file and functions to work without the user knowledge for a file to hold the prompts.
 }
