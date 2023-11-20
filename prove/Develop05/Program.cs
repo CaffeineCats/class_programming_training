42a344dc84a7d67dd342c0ab0f610f9840a3e8f0
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-11-20 14:54:05 -0300

    Moved all the submenus to EternalQuestMenu.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,12 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        EternalQuest eternalQuest = new EternalQuest();
+        Console.Clear();
 
-        eternalQuest.AppendEternalGoal();
-        eternalQuest.AppendChecklistGoal();
-        eternalQuest.AppendSimpleGoal();
+        bool keepTracking = true;
 
-        eternalQuest.DisplayGoals();
+        EternalQuestMenu eternalQuest = new EternalQuestMenu();
+
+        while (keepTracking)
+        {
+            Console.WriteLine($"\nYou have {eternalQuest.GetTotalPoints()} points\n");
+            Console.WriteLine("Menu Options:\n  1. Create New Goal\n  2. List Goals\n  3. Save Goals");
+            Console.WriteLine("  4. Load Goals\n  5. Record Event\n  6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    eternalQuest.DisplayUserGoalCreationSubMenu();
+                    break;
+                
+                case 2:
+                    eternalQuest.DisplayGoals();
+                    break;
+                
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    eternalQuest.DisplayAttemptUserGoalEventSubMenu();
+                    break;
+
+                case 6:
+                    keepTracking = false;
+                    break;
+            }
+        }
     }
 }
