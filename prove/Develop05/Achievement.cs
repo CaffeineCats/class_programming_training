535f3c534d62cf7b5489ec6ca2dfbf8fc81679ea
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-11-20 20:19:05 -0300

    Fixed

---
+++ prove/Develop05/Achievement.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+public class TotalPointsAchievement
+{
+    string _achievementName;
+    int _totalPointsGoal; 
+    bool _isComplete;
+
+    public TotalPointsAchievement(string name, int totalPointsGoal)
+    {
+        _totalPointsGoal = totalPointsGoal;
+        _isComplete = false;
+    }
+
+    public void DisplayIfPossible(int totalPoints)
+    {
+        if (!_isComplete && totalPoints >= _totalPointsGoal)
+        {
+            Console.WriteLine($"CONGRATULATIONS!!!\nYou have achieved {_achievementName}: \nBy Reaching {_totalPointsGoal} Total Points!'");
+            _isComplete = true;
+        }
+    }
+} 
