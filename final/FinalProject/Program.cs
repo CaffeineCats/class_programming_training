dd8d53b3c7786eb2f8a2246c8382b9b9450aff8d
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-11-23 16:38:20 -0300

    Intro Completed

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.Clear();
+
+        string firstParagraphIntro = "Whenever a civilization focus its attention on a goal, a strong desire to fulfill a wish, " +
+        "to make a hope come true, takes over its population. \nFor some of them, it can be te desire of overcoming death, " +
+        "some others want to extend their civilization by sending it into the stars.\nSome others, well... they worship cats.\n" +
+        "Among the timelines, one civilization managed to feed spiritual energy into these pets by worshiping them.\n" + 
+        "They became sentient, the strongest among them even developed magical powers, to conquer time and space, and the tuna stock.\n";
+
+        string secondParagraphIntro = "After countless years of allmightiness, it became increasingly bored and started playing games " +
+        "that included magical shenanigans and tricks.\nIts last source of entertainment was filling a magical looking location with " +
+        "treasure, powerful magical artifacts and advanced stolen tech.\nSome time-space rifts showed up in interesting" +
+        "locations among the mutiverse, and a peculiar assortment of creatures crossed them.\n";
+
+        string finalParagraphIntro = "That's how this fight for glory and unspeakable riches came to be.\n\n" +
+        "WELCOME TO THE MAGICAL MAYHEM BATTLEFIELD.";
+
+        Console.WriteLine(firstParagraphIntro);
+        Console.WriteLine(secondParagraphIntro);
+        Console.WriteLine(finalParagraphIntro);
     }
 }
