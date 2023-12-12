15e04780b241b5da7fb709fa6854064d19efbf94
Caffeinated Cat <luis.rqs.96@gmail.com>
2023-12-12 02:25:46 -0300

    Functionality Tested

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        //First Push to github.
+        List<Video> videos = new List<Video>();
+
+        Video videoOne = new Video("OH NO FINALS WEEK", "Luis Riquelme", 120);
+        videoOne.AppendComment(new Comment("Luis Riquelme", "He, My video."));
+        videoOne.AppendComment(new Comment("Random Banana", "GRAB THE POTASSIUM."));
+        videoOne.AppendComment(new Comment("STRESSED OMG", "Is it finals week, or your final week?"));
+
+        videos.Add(videoOne);
+
+        videos[0].DisplayVideoInformation();
+
     }
 }
